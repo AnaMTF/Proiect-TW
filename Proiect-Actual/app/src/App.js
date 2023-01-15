27e982d853f0c3bdf497581cf189e16b71e8cb8d
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-01-15 13:06:36 +0200

    Proiect final
    !merge tot, e perfect

--- Proiect-Actual/app/src/App.js
+++ Proiect-Actual/app/src/App.js
@@ -2,77 +2,41 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes as Switch, Route, Link} from 'react-router-dom';
-import CreateBT from './pagini/createBT';
-
+import createBT from './components/createBT';
+import loginBT from './components/loginBT';
+import loginME from './components/loginME';
+import createME from './components/createME';
 const Home = () => {
   return(
     <div><h1>Home</h1></div>
   )
 }
 
-const createBT = () =>
+const About = () =>
 {
   return(
-    <div><h1>createBT</h1></div>
+    <div><h1>About</h1></div>
   )
 }
 
 const App = () => {
   return (
-    // <Router>
-    /* //   <div>
-    //     <Link to="/">Home</Link>
-    //     <Link to="/createBT">CreateBT</Link>
-    //   </div>
-    //   <Switch>
-    //     <Route path="/" exact><Home /></Route>
-    //     <Route path="/createBT" exact><CreateBT /></Route>
-    //   </Switch> */
-    <div className="App">
-      <header className="App-header">
-      <head>
-        <title></title>
-        <link href="welcome.css" rel="stylesheet" type="text/css" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
-        <script src="index.js"></script>
-      </head>
-      <body>
-      <div class="container">
-        <br/><br/><br/><br/>
-        <label id="titlu" style={{"font-weight":"bold"}}>Welcome</label>
-        <br/><br/>
-        <br />
-        <div class="row">
-            <div class="col" id="grad1" style={{"border": "0.2px solid #560bad"}}>
-                <br/>
-                <label style={{"font-weight":"bold" ,"font-size":"20px", "font-family":"monospace", "color":"#560bad"}}>Want to upload a project?</label>
-                <br/><br/>
-                <button class="btn btn-primary m-2" id="createMembruEchipa" onclick="createMembruEchipa()">Create MembruEchipa account</button>
-                <br/><br/>
-                <button class="btn btn-primary m-3" id="loginMembruEchipa" onclick="loginMembruEchipa()">Log in as MembruEchipa</button>
-                <br/>
-            </div>
-            <div class="col-1"></div>
-            <div class="col" id="grad1" style={{"border": "0.2px solid #560bad"}}>
-                <br/>
-                <label style={{"font-weight":"bold" ,"font-size":"20px", "font-family":"monospace", "color":"#560bad"}}>Want to test some bugs?</label>
-                <br/><br/>
-                {/* <Link to="/createBT"> */}
-                  <button class="btn btn-primary m-2" id="createBugTester" onclick="createBugTester()">Create BugTester account</button>
-                {/* </Link> */}
-                <br/><br/>
-                <button class="btn btn-primary m-3" id="loginBugTester" onclick="loginBugTester()">Log in as BugTester</button>
-                <br/>
-            </div>
-        </div>
-    </div>
-      </body>
-      </header>
-    </div> 
-    // <Route path='/createBT' exact>
-    //   <CreateBT></CreateBT>
-    // </Route>
-    // </Router>
+    <Router>
+      <div>
+        <a href="./components/createBT">createBT</a>
+        <a href="./components/loginBT">loginBT</a>
+        <a href="./components/createME">createME</a>
+        <a href="./components/loginME">loginME</a>
+      </div>  
+
+      <Switch>
+        <Route path="/createBT" component={createBT} />
+        <Route path="/loginBT" component={loginBT} />
+        <Route path="/createME" component={createME} />
+        <Route path="/loginME" component={loginME} />
+      </Switch>
+    </Router>
+    
   );
 }
 
---
+++ Proiect-Actual/app/src/components/loginBT.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const LoginBT = () => {
+    return (
+        <div>
+            <head>
+                <title></title>
+                <link href="loginBT.css" rel="stylesheet" type="text/css" />
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+                <script src="index.js"></script>
+            </head>
+            <body>
+
+            
+            <h1>Log in Bug Tester</h1>
+            <div class="container">
+            <br /><br /><br /><br />
+            <label id="titlu" style="font-weight:bold;">Log in Bug Tester</label>
+            <br /><br />
+            <br />
+            <form>
+                <label style={{"font-weight":"bold", "font-size":"20px", "font-family":"monospace", "color":"#560bad"}}>Email:</label>
+                <input type="email" />
+                <br />
+                <label style={{"font-weight":"bold", "font-size":"20px", "font-family":"monospace", "color":"#560bad"}}>Password:</label>
+                <input type="password" />
+                <br />
+                <button class="btn btn-primary m-3" id="loginMembruEchipa" onclick="login()">Log in</button>
+                <button>Log in</button>
+            </form>
+            </div>
+        </body>
+        </div>
+    );
+};
+
+export default LoginBT;
